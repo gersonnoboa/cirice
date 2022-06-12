452f505e0d496c495f82083733bb5010a5e57f93
Gerson Noboa <gnoboa@protonmail.com>
2022-06-12 21:37:58 +0300

    Add SDK tests for face extraction

--- CiriceSDK/Tests/CiriceSDKTests/CiriceSDKTests.swift
+++ CiriceSDK/Tests/CiriceSDKTests/CiriceSDKTests.swift
@@ -33,4 +33,42 @@ final class CiriceSDKTests: XCTestCase {
             XCTAssertTrue(error is CiriceError)
         }
     }
+
+    func testGetFaceSuccess() async {
+        let mock = ImageRecognitionCapableSuccessMock()
+        let interactor = FaceExtractorInteractor(imageRecognitionCapable: mock)
+
+        let sdk = CiriceSDK(faceExtractorInteractable: interactor)
+        guard let image = UIImage(named: "max", in: Bundle.module, with: .none) else {
+            XCTFail("Image not loaded")
+            return
+        }
+        let request = FaceExtractorRequest(image: image)
+
+        do {
+            let response = try await sdk.getFaces(using: request)
+            XCTAssertTrue(response.faceImages.count == 1)
+        } catch {
+            XCTFail("Should succeed")
+        }
+    }
+
+    func testGetFacesError() async {
+        let mock = ImageRecognitionCapableFailureMock()
+        let interactor = FaceExtractorInteractor(imageRecognitionCapable: mock)
+
+        let sdk = CiriceSDK(faceExtractorInteractable: interactor)
+        guard let image = UIImage(named: "family", in: Bundle.module, with: .none) else {
+            XCTFail("Image not loaded")
+            return
+        }
+        let request = FaceExtractorRequest(image: image)
+
+        do {
+            let _ = try await sdk.getFaces(using: request)
+            XCTFail("Should fail")
+        } catch {
+            XCTAssertTrue(error is CiriceError)
+        }
+    }
 }
